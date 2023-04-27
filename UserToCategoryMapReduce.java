785bc8ccddeec38221e1cee54a53184936e1a63d
zxcnewman314 <105270694+zxcnewman314@users.noreply.github.com>
2023-04-27 18:22:16 +0800

    Add files via upload

--- UserToCategoryMapReduce.java
+++ UserToCategoryMapReduce.java
@@ -1,7 +1,9 @@
+package map;
 import java.io.IOException;
 import java.util.StringTokenizer;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
@@ -20,8 +22,12 @@ public class UserToCategoryMapReduce {
         public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
             StringTokenizer tokenizer = new StringTokenizer(value.toString(), "\t");
             String userId = tokenizer.nextToken();
+            int i=0;
             while (tokenizer.hasMoreTokens()) {
-                category.set(tokenizer.nextToken());
+              String t=tokenizer.nextToken();
+              if(!t.contains("c"))
+                  continue;
+                category.set(t);
                 user.set(userId);
                 context.write(category, user);
             }
@@ -45,14 +51,19 @@ public class UserToCategoryMapReduce {
 
     public static void main(String[] args) throws Exception {
         Configuration conf = new Configuration();
+        conf.set("fs.default.name","hdfs://localhost:9000");
         Job job = Job.getInstance(conf, "user-to-category");
         job.setJarByClass(UserToCategoryMapReduce.class);
         job.setMapperClass(UserToCategoryMapper.class);
         job.setReducerClass(UserToCategoryReducer.class);
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(Text.class);
-        FileInputFormat.addInputPath(job, new Path(args[0]));
-        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        FileInputFormat.addInputPath(job, new Path("/user/hadoop/map/shop"));
+        Path out=new Path("/user/hadoop/map/shopOutput");
+        FileSystem fs=out.getFileSystem(conf);
+        if(fs.exists(out))
+            fs.delete(out,true);
+        FileOutputFormat.setOutputPath(job, new Path("/user/hadoop/map/shopOutput"));
         System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 }
